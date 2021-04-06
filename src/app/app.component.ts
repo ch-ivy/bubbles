e7ce17b86476bc384b3ec64c911a08a53a84861f
ch-ivy <chivy1221@gmail.com>
2021-04-06 08:53:51 +0300

    project initiated

---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  bub1: string;
+  bub2: string;
+
+  bbl1 = 150;
+  bbl2 = 150;
+  max: string = 'Consumption';
+  min: string = 'Generation';
+  percentage: any = 100;
+
+  toggle() {
+    const bub1 = parseFloat(this.bub1);
+    const bub2 = parseFloat(this.bub2);
+    const percentage = Math.min(bub1, bub2) / Math.max(bub1, bub2);
+    if (bub1 > bub2) {
+      this.bbl2 = Math.round(percentage * 100);
+      this.bbl1 = 150;
+      this.max = 'Consumption';
+      this.min = 'Generation';
+    }
+    if (bub1 < bub2) {
+      this.bbl1 = Math.round(percentage * 100);
+      this.bbl2 = 150;
+      this.min = 'Consumption';
+      this.max = 'Generation';
+    }
+    if (bub1 == bub2) {
+      this.bbl1 = 150;
+      this.bbl2 = 150;
+      this.max = 'Consumption';
+      this.min = 'Generation';
+    }
+    this.percentage = (percentage * 100).toFixed();
+    console.log(this.bbl1, this.bbl2);
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { LayoutModule } from '@angular/cdk/layout';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, BrowserAnimationsModule, LayoutModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
