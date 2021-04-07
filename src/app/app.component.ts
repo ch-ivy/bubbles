ea5636308e7445d68517b5c544b619143718e3b3
ch-ivy <chivy1221@gmail.com>
2021-04-07 06:38:17 +0300

    resized boxsize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,13 @@ export class AppComponent implements OnInit {
   left_input: string = '20';
   right_input: string = '16';
 
-  left_bbl_size = 200;
-  right_bbl_size = 200;
+  left_bbl_size = 80;
+  right_bbl_size = 80;
   max: string = 'Consumption';
   min: string = 'Generation';
   percentage: any = 100;
   minSize: number; // Both bubbles parents boxes adgust width to minimum bubble size to maintain close positions
-  m_box_auto: 'g' | 'c' | ''; // determines which box margins top and bottom becomes auto
+  righ_bbl_top = false; // Right bubble adds removes top value when left bubble is smaller
 
   ngOnInit() {
     this.toggle();
@@ -27,28 +27,28 @@ export class AppComponent implements OnInit {
     const percentage =
       Math.min(left_input, right_input) / Math.max(left_input, right_input);
     if (left_input > right_input) {
-      this.right_bbl_size = Math.round(percentage * 200);
-      this.left_bbl_size = 200;
+      this.right_bbl_size = Math.round(percentage * 80);
+      this.left_bbl_size = 80;
       this.max = 'Consumption';
       this.min = 'Generation';
       this.minSize = this.right_bbl_size;
-      this.m_box_auto = 'g';
+      this.righ_bbl_top = false;
     }
     if (left_input < right_input) {
-      this.left_bbl_size = Math.round(percentage * 200);
-      this.right_bbl_size = 200;
+      this.left_bbl_size = Math.round(percentage * 80);
+      this.right_bbl_size = 80;
       this.min = 'Consumption';
       this.max = 'Generation';
       this.minSize = this.left_bbl_size;
-      this.m_box_auto = 'c';
+      this.righ_bbl_top = true;
     }
     if (left_input == right_input) {
-      this.left_bbl_size = 200;
-      this.right_bbl_size = 200;
+      this.left_bbl_size = 80;
+      this.right_bbl_size = 80;
       this.max = 'Consumption';
       this.min = 'Generation';
-      this.minSize = 150;
-      this.m_box_auto = '';
+      this.minSize = 80;
+      this.righ_bbl_top = false;
     }
     this.percentage = (percentage * 100).toFixed();
     console.log(this.left_bbl_size, this.right_bbl_size);
